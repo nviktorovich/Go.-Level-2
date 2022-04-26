9e48a46437f3b06b93ecebd9856b08e44c0f3b0a
NViktorovich <nvmaslenko@gmail.com>
2022-04-26 23:15:30 +0500

    use go fmt for all executed go files

--- HW1/Task1/main.go
+++ HW1/Task1/main.go
@@ -1,24 +1,21 @@
-// Для закрепления навыков отложенного вызова функций, напишите программу, содержащую вызов функции, 
-// которая будет создавать паническую ситуацию неявно. 
-// Затем создайте отложенный вызов, который будет обрабатывать эту паническую ситуацию и, в частности, печатать предупреждение в консоль. 
+// Для закрепления навыков отложенного вызова функций, напишите программу, содержащую вызов функции,
+// которая будет создавать паническую ситуацию неявно.
+// Затем создайте отложенный вызов, который будет обрабатывать эту паническую ситуацию и, в частности, печатать предупреждение в консоль.
 // Критерием успешного выполнения задания является то, что программа не завершается аварийно ни при каких условиях.
-
 
 package main
 
-import(
+import (
 	"fmt"
 )
 
 func main() {
 	defer fmt.Println("end of pogram")
 	defer func() {
-     if rcvr := recover(); rcvr != nil {
-        fmt.Println("recovered", rcvr)
-     }
+		if rcvr := recover(); rcvr != nil {
+			fmt.Println("recovered", rcvr)
+		}
 	}()
-
-	
 
 	var s []int = []int{1, 2, 3, 4, 5}
 	var userIdx int
@@ -26,10 +23,10 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	
+
 	fmt.Println(GetNumberFromSliceToIndex(userIdx, s))
 }
 
-func GetNumberFromSliceToIndex(idx int, s []int) int{
+func GetNumberFromSliceToIndex(idx int, s []int) int {
 	return s[idx]
 }
